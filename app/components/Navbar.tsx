72c06626c253fa5b29ce23980bcb17fbf15c60bc
Dave Faber <dave.faber@gusto.com>
2024-05-18 12:20:22 -0500

    Chapter 6

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,45 @@
 import Link from "next/link";
-import Search from "./Search";
+import { FaYoutube, FaGithub, FaTwitter, FaLaptop } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
-      <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-        <Link href="/">TestProject</Link>
-      </h1>
-      <Search />
+    <nav className="bg-slate-600 p-4 stick top-0 drop-shadow-xl z-10">
+      <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+          <Link
+            href="/"
+            className="text-white/90 no-underline hover:text-white"
+          >
+            Home
+          </Link>
+        </h1>
+        <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.google.com"
+          >
+            <FaYoutube />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.google.com"
+          >
+            <FaTwitter />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.google.com"
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.google.com"
+          >
+            <FaLaptop />
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Posts from "./components/Posts";
 
 export default function Home() {
-  return <main className={inter.className}></main>;
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome{" "}
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Dave</span>.
+        </span>
+      </p>
+      <Posts />
+    </main>
+  );
 }

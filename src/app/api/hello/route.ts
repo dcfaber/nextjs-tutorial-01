dd4aba086da58373f94aae4c79544252341b00b0
Dave Faber <dave.faber@gusto.com>
2024-05-18 18:43:21 -0500

    Chapter 8

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,3 @@
-import { NextResponse } from "next/server";
-
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "Hello, Next.js!" });
+  return new Response('Hello, Next.js!')
 }

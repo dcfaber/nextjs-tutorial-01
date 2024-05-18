0f49d049f3bb0aea694a8c6111cf4efb1f7811b6
Dave Faber <dave.faber@gusto.com>
2024-05-18 10:23:09 -0500

    Chapter 4

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,8 +1,11 @@
 import getUser from "../../lib/getUser";
 import getUserPosts from "../../lib/getUserPosts";
+import getAllUsers from "../../lib/getAllUsers";
 import UserPosts from "./components/UserPosts";
 import type { Metadata } from "next";
 import { Suspense } from "react";
+
+import { notFound } from "next/navigation";
 
 type Params = {
   params: { userId: string };
@@ -13,8 +16,15 @@ export async function generateMetadata({
 }: Params): Promise<Metadata> {
   const user = await getUser(userId);
 
+  if (!user?.name) {
+    return {
+      title: "User not found",
+    };
+  }
+
   return {
     title: user.name,
+    description: `This is the page of ${user.name}`,
   };
 }
 
@@ -23,6 +33,10 @@ export default async function UserPage({ params: { userId } }: Params) {
   const userPostData: Promise<Post[]> = getUserPosts(userId);
 
   const user = await userData;
+
+  if (!user?.name) {
+    return notFound();
+  }
 
   return (
     <>
@@ -33,4 +47,13 @@ export default async function UserPage({ params: { userId } }: Params) {
       </Suspense>
     </>
   );
+}
+
+export async function generateStaticParams() {
+  const usersData: Promise<User[]> = getAllUsers();
+  const users = await usersData;
+
+  return users.map((user) => ({
+    userId: user.id.toString(),
+  }));
 }

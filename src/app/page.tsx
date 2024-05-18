0a43289d38d39bddaad20e18100ff53fa9f55426
Dave Faber <dave.faber@gusto.com>
2024-05-18 18:12:43 -0500

    Chapter 7

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className="text-3xl grid place-content-center min-h-screen">
+        Hello World!
+      </h1>
+    </main>
+  );
+}
